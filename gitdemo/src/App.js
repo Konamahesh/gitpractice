44a471f73a7aacb479eec31de544e9ba34b9db11
Konamahesh <132975920+Konamahesh@users.noreply.github.com>
2023-09-29 14:40:18 +0530

    text changesd

--- gitdemo/src/App.js
+++ gitdemo/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          demo practice purpose we doing
         </a>
       </header>
     </div>
